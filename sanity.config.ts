0e72273a5df441dd20dbc9c2874056a266adbc28
M-Sahin <murat.sa25@gmail.com>
2025-11-01 23:22:18 +0100

    fix: add peer dependency flag for immer in package-lock.json refactor: clean up whitespace in sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,11 +6,10 @@ import { projectType } from './sanity/schemaTypes/project'
 export default defineConfig({
   name: 'default',
   title: 'SmartWheels',
-
   projectId: 'ckmy7d6l',
   dataset: 'production',
   apiVersion: '2024-11-01',
-
+  basePath: '/studio',
   plugins: [structureTool(), visionTool()],
   schema: {
     types: [projectType],
